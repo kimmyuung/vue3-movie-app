1a86439155a2af0a4600362d8aa782e8501bdc94
kimmyungho <kimmyungho1003@naver.com>
2023-10-25 21:52:20 +0900

    기본 라우터 설정

---
+++ src/routes/index.js
@@ -0,0 +1,22 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import Home from './Home'
+import About from './About'
+
+export default createRouter({
+    // History 사용시에는 서버에 세팅 필요
+    
+    // Hash
+    // https://google.com/#/search
+    history: createWebHashHistory(),
+    // pages
+    // https://google.com/about -> about path's component run
+    routes: [
+        {
+            path: '/',
+            component: Home
+        }, {
+            path: '/about',
+            component: About
+        }
+    ]
+})

2e4f4cf96b71845ba745bd8a0614b950c1966c0f
kimmyungho <kimmyungho1003@naver.com>
2023-10-31 19:58:01 +0900

    Headline

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,5 +1,23 @@
 <template>
-  <RouterLink to="/">
-    OMDbAPI.com
+  <RouterLink
+    to="/"
+    class="logo">
+    <span>OMDbAPI.com</span>
   </RouterLink>
 </template>
+
+<style lang="scss" scoped>
+@import "~/scss/main";
+
+.logo {
+    font-family: "Oswald", sans-serif;
+    font-size: 20px;
+    color: $black;
+    &:hover {
+        color: $black;
+    }
+    span {
+        color: $primary;
+    }
+}
+</style>
